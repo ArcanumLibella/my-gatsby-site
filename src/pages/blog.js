d7a786f3a0cd3ad9c037e107900068329f2017bf
Fiona <fiona.roux@hetic.net>
2022-03-09 12:57:03 +0100

    feat: display content from each blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,40 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
 import Layout from '../components/layout'
 
 const BlogPage = ({data}) => {
 	return (
 		<Layout pageTitle="My Blog Posts">
-			<ul>
-					{data.allFile.nodes.map(post => {
-						return (
-							<li>{post.name}</li>
-						)
-					})}
-			</ul>
+			{data.allMdx.nodes.map(post => {
+				return (
+					<article key={post.id}>
+						<h2>{post.frontmatter.title}</h2>
+            <p>Posted: {post.frontmatter.date}</p>
+						<MDXRenderer>
+							{ post.body }
+						</MDXRenderer>
+					</article>
+				)
+			})}
 		</Layout>
 	)
 }
 
 export const query = graphql`
-	query {
-		allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-			nodes {
-				name
-			}
-		}
-	}
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
+      }
+    }
+  }
 `
+
 export default BlogPage
