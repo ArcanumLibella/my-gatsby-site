82e2276f845d2114370dff7419b7ccbf3d7d8908
Fiona <fiona.roux@hetic.net>
2022-03-09 19:02:04 +0100

    feat: add dynamic images

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,13 +1,20 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import Layout from '../../components/layout'
 
 const BlogPost = ({data}) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -17,14 +24,21 @@ const BlogPost = ({data}) => {
 
 export const GetPostData = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
+      hero_image {
+				childImageSharp {
+					gatsbyImageData
+        }
       }
-      body
+      hero_image_alt
+      hero_image_credit_text
     }
+    body
   }
+}
 `
 
 export default BlogPost
